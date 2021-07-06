ed93106c51264b9d9f11f4d42ece4227575463ae
DravenRedgrave <77494310+DravenRedgrave@users.noreply.github.com>
2021-07-06 20:37:39 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { lazy,Suspense } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import * as ROUTES from './constants/routes';
+import useAuthListener from './hooks/use-auth-listener';
+import UserContext from './context/user';
 const Login = lazy(() => import('./pages/login'));
 const SignUp = lazy(() => import('./pages/signup'));
 const Dashboard = lazy(() => import('./pages/dashboard'));
 const NotFound = lazy(() => import('./pages/not-found'));
 export default function App() {
+  const {user} = useAuthListener();
   return (
+    <UserContext.Provider value={{user}}>
     <Router>
       <Suspense fallback={<p>Loading...</p>}>
       <Switch>
@@ -17,5 +21,6 @@ export default function App() {
       </Switch>
       </Suspense>
     </Router>
+    </UserContext.Provider>
   );
 }
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -65,22 +65,6 @@ export default function SignUp() {
           {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
 
           <form onSubmit={handleSignUp} method="POST">
-          <input
-              aria-label="Enter your email address"
-              type="text"
-              placeholder="Email address"
-              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-              onChange={({ target }) => setEmailAddress(target.value)}
-              value={emailAddress}
-            />
-            <input
-              aria-label="Enter your password"
-              type="password"
-              placeholder="Password"
-              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-              onChange={({ target }) => setPassword(target.value)}
-              value={password}
-            />
             <input
               aria-label="Enter your username"
               type="text"
@@ -96,6 +80,22 @@ export default function SignUp() {
               className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
               onChange={({ target }) => setFullName(target.value)}
               value={fullname}
+            />
+              <input
+              aria-label="Enter your email address"
+              type="text"
+              placeholder="Email address"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setEmailAddress(target.value)}
+              value={emailAddress}
+            />
+            <input
+              aria-label="Enter your password"
+              type="password"
+              placeholder="Password"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setPassword(target.value)}
+              value={password}
             />
             <button
               disabled={isInvalid}
